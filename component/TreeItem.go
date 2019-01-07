1c07b63877ede0883b7779e28bb253c7e33dadbc
zhaoshuai3 <1115870494@qq.com>
2019-01-07 20:19:27 +0800

    修改树形菜单

--- component/TreeItem.go
+++ component/TreeItem.go
@@ -25,38 +25,44 @@ type TreeUnit struct{
 
 func CreateTree() *gtk.Box{
 	vBox,_ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL,0)
+	//gtk.ScrollbarNew(gtk.ORIENTATION_VERTICAL,nil)
 	treeView, err := gtk.TreeViewNew()
 	if err != nil {
 		log.Fatal("Unable to create tree view:", err)
 	}
 
-	treeView.AppendColumn(createImageColumn("", COLUMN_ICON))
-	treeView.AppendColumn(createTextColumn("", COLUMN_TEXT))
+	treeView.AppendColumn(createItemColumn("", COLUMN_ICON))
+	treeView.SetHExpand(false)
 
-	// Creating a tree store. This is what holds the data that will be shown on our tree view.
 	treeStore, err := gtk.TreeStoreNew(glib.TYPE_OBJECT, glib.TYPE_STRING)
 	if err != nil {
 		log.Fatal("Unable to create tree store:", err)
 	}
 	treeView.SetModel(treeStore)
 
-	spixbuf,_ := gdk.PixbufNewFromFile("/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/index.png")
-	pixbuf,_ := spixbuf.ScaleSimple(30,30,gdk.INTERP_HYPER)
+	indexspixbuf,_ := gdk.PixbufNewFromFile("/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/index.png")
+	indexpixbuf,_ := indexspixbuf.ScaleSimple(20,20,gdk.INTERP_TILES)
+
+	conspixbuf,_ := gdk.PixbufNewFromFile("/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	conpixbuf,_ := conspixbuf.ScaleSimple(20,20,gdk.INTERP_TILES)
+
+	docspixbuf,_ := gdk.PixbufNewFromFile("/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
+	docpixbuf,_ := docspixbuf.ScaleSimple(20,20,gdk.INTERP_TILES)
 
 	// todo data implements after
-	iter1 := addRow(treeStore, pixbuf, "Testsuite 1")
-	iter2 := addSubRow(treeStore, iter1, pixbuf, "test1-1")
-	iter2 = addSubRow(treeStore, iter1, pixbuf, "test1-2")
-	addSubRow(treeStore, iter2, pixbuf, "test1-2-1")
-	addSubRow(treeStore, iter2, pixbuf, "test1-2-2")
-	addSubRow(treeStore, iter2, pixbuf, "test1-2-3")
-	iter2 = addSubRow(treeStore, iter1, pixbuf, "test1-3")
-	iter1 = addRow(treeStore, pixbuf, "Testsuite 2")
-	iter2 = addSubRow(treeStore, iter1, pixbuf, "test2-1")
-	iter2 = addSubRow(treeStore, iter1, pixbuf, "test2-2")
-	iter2 = addSubRow(treeStore, iter1, pixbuf, "test2-3")
-	addSubRow(treeStore, iter2, pixbuf, "test2-3-1")
-	addSubRow(treeStore, iter2, pixbuf, "test2-3-2")
+	iter1 := addRow(treeStore, conpixbuf, "127.0.0.1                            ")
+	iter2 := addSubRow(treeStore, iter1, indexpixbuf, "test1-1")
+	iter2 = addSubRow(treeStore, iter1, indexpixbuf, "test1-2")
+	addSubRow(treeStore, iter2, docpixbuf, "test1-2-1")
+	addSubRow(treeStore, iter2, docpixbuf, "test1-2-2")
+	addSubRow(treeStore, iter2, docpixbuf, "test1-2-3")
+	iter2 = addSubRow(treeStore, iter1, indexpixbuf, "test1-3")
+	iter1 = addRow(treeStore, conpixbuf, "10.70.93.52                           ")
+	iter2 = addSubRow(treeStore, iter1, indexpixbuf, "test2-1")
+	iter2 = addSubRow(treeStore, iter1, indexpixbuf, "test2-2")
+	iter2 = addSubRow(treeStore, iter1, indexpixbuf, "test2-3")
+	addSubRow(treeStore, iter2, docpixbuf, "test2-3-1")
+	addSubRow(treeStore, iter2, docpixbuf, "test2-3-2")
 
 	// 处理选择
 	selection, err := treeView.GetSelection()
@@ -68,52 +74,34 @@ func CreateTree() *gtk.Box{
 
 	// treeview
 	treeView.SetSizeRequest(common.WindowLeftWidth,common.WindowLeftHeight)
-	treeView.SetMarginTop(20)
+	treeView.SetMarginTop(5)
 	treeView.SetEnableSearch(true)
-	treeView.SetSearchColumn(2)
+	treeView.SetSearchColumn(1)
+	treeView.SetHAlign(gtk.ALIGN_START)
 
+	searchEntity,_ := gtk.EntryNew()
+	searchEntity.SetSizeRequest(common.WindowLeftWidth,30)
+	treeView.SetSearchEntry(searchEntity)
 
 	vBox.Add(treeView)
 	vBox.SetSizeRequest(common.WindowLeftWidth,common.WindowLeftHeight)
 	return vBox
 }
 
-// Add a column to the tree view (during the initialization of the tree view)
-// We need to distinct the type of data shown in either column.
-func createTextColumn(title string, id int) *gtk.TreeViewColumn {
-	// In this column we want to show text, hence create a text renderer
-	cellRenderer, err := gtk.CellRendererTextNew()
-	if err != nil {
-		log.Fatal("Unable to create text cell renderer:", err)
-	}
+func createItemColumn(title string, id int) *gtk.TreeViewColumn {
+	cellpb,_ := gtk.CellRendererPixbufNew()
+	col,_ := gtk.TreeViewColumnNewWithAttribute(title, cellpb, "pixbuf",id)
+	cell,_ := gtk.CellRendererTextNew()
+	col.PackEnd(cell, false)
+	col.AddAttribute(cell, "text", 1)
+	col.SetFixedWidth(common.WindowLeftWidth)
 
-	// Tell the renderer where to pick input from. Text renderer understands
-	// the "text" property.
-	column, err := gtk.TreeViewColumnNewWithAttribute(title, cellRenderer, "text", id)
-	if err != nil {
-		log.Fatal("Unable to create cell column:", err)
-	}
-	column.SetSizing(gtk.TREE_VIEW_COLUMN_FIXED)
-	return column
-}
-
-func createImageColumn(title string, id int) *gtk.TreeViewColumn {
-	// In this column we want to show image data from Pixbuf, hence
-	// create a pixbuf renderer
-	cellRenderer, err := gtk.CellRendererPixbufNew()
-	if err != nil {
-		log.Fatal("Unable to create pixbuf cell renderer:", err)
-	}
-
-	// Tell the renderer where to pick input from. Pixbuf renderer understands
-	// the "pixbuf" property.
-	column, err := gtk.TreeViewColumnNewWithAttribute(title, cellRenderer, "pixbuf", id)
-	if err != nil {
-		log.Fatal("Unable to create cell column:", err)
-	}
-	column.SetSizing(gtk.TREE_VIEW_COLUMN_FIXED)
-	column.SetFixedWidth(120)
-	return column
+	//cellpb,_ := gtk.CellRendererPixbufNew()
+	//cell,_ := gtk.CellRendererTextNew()
+	//col,_ := gtk.TreeViewColumnNewWithAttribute(title, cell, "text",1)
+	//col.PackEnd(cellpb, true)
+	//col.AddAttribute(cellpb, "pixbuf", id)
+	return col
 }
 
 // Append a sub row to the tree store for the tree view
