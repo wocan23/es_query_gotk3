f9430fe2b138db39415a22295166986c1b2cc8db
zhaoshuai3 <1115870494@qq.com>
2019-01-08 17:40:27 +0800

    level

--- component/Tree.go
+++ component/Tree.go
@@ -5,7 +5,6 @@ import (
 	"../helper"
 	"../common"
 	"C"
-	"fmt"
 )
 
 type TreeData struct{
@@ -13,6 +12,8 @@ type TreeData struct{
 	subItems []*TreeData //子数据
 	isSubShow bool // 是否展示子标签
 	isRoot bool
+
+	parent *TreeData
 }
 
 type TreeItemData struct{
@@ -34,6 +35,7 @@ func (treeData *TreeData)AddSubItems(subTreeData *TreeData){
 		treeData.subItems = make([]*TreeData,0)
 	}
 	treeData.subItems = append(treeData.subItems, subTreeData)
+	subTreeData.parent = treeData
 }
 
 func CreateTreeData()*TreeData{
@@ -42,6 +44,8 @@ func CreateTreeData()*TreeData{
 
 func CreateTreeByData(data *TreeData,getWidget func(data *TreeData,root *gtk.Box,parent *gtk.Box)gtk.IWidget) *gtk.Box {
 	box, _ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL, 0)
+	box.SetSizeRequest(common.WindowLeftWidth,common.WindowLeftHeight)
+	box.SetBorderWidth(5)
 	flushTree(box,data,box,getWidget)
 	return box
 
@@ -50,16 +54,102 @@ func CreateTreeByData(data *TreeData,getWidget func(data *TreeData,root *gtk.Box
 func flushTree(root *gtk.Box,data *TreeData,parent *gtk.Box,getWidget func(data *TreeData,root *gtk.Box,parent *gtk.Box)gtk.IWidget) {
 	// 第一层直接遍历子
 	curBox,_ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL,0)
-	if !data.isRoot{
-		widget := getWidget(data,root,curBox)
-		curBox.Add(widget)
-	}
-	if data.isRoot {
-		showSubItems(data,root,curBox)
 
-	}
+	widget := getWidget(data,root,curBox)
+	curBox.Add(widget)
+
 	parent.Add(curBox)
 }
+
+func GetWidget(data *TreeData,root *gtk.Box,current *gtk.Box) gtk.IWidget{
+	text := data.item.data["text"]
+	imagePath := data.item.data["imagePath"]
+	btn,_ := gtk.EventBoxNew()
+	btn.SetSizeRequest(100,20)
+
+
+	btn.SetHExpand(false)
+
+	box,_ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL,5)
+	var level = checkLevel(data)
+	for i := 0; i < level; i++{
+		_,height := box.GetSizeRequest()
+		label,_ := gtk.LabelNew(">")
+		label.SetSizeRequest(10,height)
+		box.Add(label)
+	}
+	box.SetHExpand(false)
+
+	image := helper.CreateImage(common.TreeItemWidth,common.TreeItemHeight,imagePath)
+
+
+	textV,_ := gtk.LabelNew(text)
+	textV.SetTooltipText(text)
+
+
+	entity,_ := gtk.EntryNew()
+	entity.SetTooltipText(text)
+	entity.SetVisible(false)
+
+	box.Add(image)
+	box.Add(textV)
+
+	// 点击事件
+	btn.Connect("button_press_event", func(e *gtk.EventBox) {
+		data.isSubShow = !data.isSubShow
+		if data.isSubShow{
+			showSubItems(data,root,current)
+		}else{
+			unShowSubItems(current)
+		}
+	})
+	btn.Add(box)
+	btn.ShowAll()
+
+	return btn
+}
+
+func showSubItems(data *TreeData,root *gtk.Box,current *gtk.Box){
+	for _, e := range data.subItems {
+		pBox,_ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL,0)
+
+		//_,height := pBox.GetSizeRequest()
+		//label,_ := gtk.LabelNew(">")
+		//label.SetSizeRequest(10,height)
+		//pBox.Add(label)
+
+		widget := GetWidget(e,root,pBox)
+
+		pBox.Add(widget)
+		pBox.SetMarginStart(10)
+		current.Add(pBox)
+	}
+	current.ShowAll()
+}
+
+func unShowSubItems(current *gtk.Box){
+	list := current.GetChildren()
+	var length = list.Length()
+	var i uint
+	for i = 1 ; i < length; i ++{
+		item := list.NthData(i)
+		widget := item.(*gtk.Widget)
+		widget.Destroy()
+	}
+	current.Show()
+}
+
+func checkLevel(data *TreeData)int{
+	var i = 0
+	parent := data.parent
+	for parent != nil{
+		parent = parent.parent
+		i ++
+	}
+	return i
+}
+
+
 
 func TreeTest()*gtk.Box{
 
@@ -81,23 +171,23 @@ func TreeTest()*gtk.Box{
 	d1.SetProperty("text","d1")
 	d1.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
 	d11.SetProperty("text","d11")
-	d11.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d11.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/index.png")
 	d12.SetProperty("text","d12")
-	d12.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d12.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/index.png")
 	d13.SetProperty("text","d13")
-	d13.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d13.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/index.png")
 	d111.SetProperty("text","d111")
-	d111.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d111.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 	d121.SetProperty("text","d121")
-	d121.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d121.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 	d122.SetProperty("text","d122")
-	d122.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d122.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 	d131.SetProperty("text","d131")
-	d131.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d131.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 	d132.SetProperty("text","d132")
-	d132.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d132.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 	d133.SetProperty("text","d133")
-	d133.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/conn.png")
+	d133.SetProperty("imagePath","/Users/zhaoshuai/Documents/go_workspace_wocan/es_query_gotk3/images/doc.png")
 
 	d11.AddSubItems(d111)
 
@@ -115,72 +205,4 @@ func TreeTest()*gtk.Box{
 
 	return CreateTreeByData(d1,GetWidget)
 
-}
-
-
-func GetWidget(data *TreeData,root *gtk.Box,current *gtk.Box) gtk.IWidget{
-	text := data.item.data["text"]
-	imagePath := data.item.data["imagePath"]
-	btn,_ := gtk.ButtonNew()
-	btn.SetSizeRequest(100,20)
-	btn.SetHExpand(false)
-
-	box,_ := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL,5)
-	box.SetHExpand(false)
-
-
-	image := helper.CreateImage(common.TreeItemWidth,common.TreeItemHeight,imagePath)
-
-
-	textV,_ := gtk.LabelNew(text)
-	textV.SetTooltipText(text)
-
-
-	entity,_ := gtk.EntryNew()
-	entity.SetTooltipText(text)
-	entity.SetVisible(false)
-
-	box.Add(image)
-	box.Add(textV)
-	//box.Add(entity)
-	//uuid := common2.GetUuid()
-	//btn.SetProperty("id",uuid)
-
-	// 点击事件
-	btn.Connect("clicked", func(e *gtk.Button) {
-		data.isSubShow = !data.isSubShow
-		if data.isSubShow{
-			showSubItems(data,root,current)
-		}else{
-			unShowSubItems(current)
-		}
-	})
-	btn.Add(box)
-	btn.ShowAll()
-
-	return btn
-}
-
-func showSubItems(data *TreeData,root *gtk.Box,current *gtk.Box){
-	for _, e := range data.subItems {
-		pBox,_ := gtk.BoxNew(gtk.ORIENTATION_VERTICAL,0)
-		pBox.Add(GetWidget(e,root,pBox))
-		pBox.SetMarginStart(10)
-		current.Add(pBox)
-	}
-	current.ShowAll()
-	fmt.Println(current)
-}
-
-func unShowSubItems(current *gtk.Box){
-	list := current.GetChildren()
-	fmt.Println(current)
-	var length = list.Length()
-	var i uint
-	for i = 1 ; i < length; i ++{
-		item := list.NthData(i)
-		widget := item.(*gtk.Widget)
-		widget.Destroy()
-	}
-	current.Show()
 }
